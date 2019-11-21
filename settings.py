86553506304c87e7bc0c2f06f92ad48fad20c9fd
Sophie Miller <38506919+sophiekaymiller@users.noreply.github.com>
2019-11-21 00:49:45 -0500

    Delete settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/SKMPRO/Documents/GitHub/API_Project/database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
